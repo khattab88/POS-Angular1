51b4db051044f756a4644f3d400ec635d5f58765
Mahmoud Khattab <m_khatab_88@hotmail.com>
2017-10-06 23:50:19 +0200

    add ng-repeat to custom directive

--- app/home/home.js
+++ app/home/home.js
@@ -2,58 +2,68 @@
 
 angular.module('myApp.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'home/home.html',
+      controller: 'HomeCtrl'
+    });
+  }])
 
-.controller('HomeCtrl', ["$scope", "InvoiceSvc", "SharedSvc", function($scope, InvoiceSvc, SharedSvc) {
+  .controller('HomeCtrl', ["$scope", "InvoiceSvc", "SharedSvc", function ($scope, InvoiceSvc, SharedSvc) {
 
-  $scope.title = "Home Page";
+    $scope.title = "Home Page";
 
-  // $scope.title = 'Click me to expand';
-  // $scope.text = 'Hi there folks, I am the content'
-  // + 'that was hidden but is now shown.';
+    // $scope.title = 'Click me to expand';
+    // $scope.text = 'Hi there folks, I am the content'
+    // + 'that was hidden but is now shown.';
 
-  // $scope.myText = 'Not Selected';
-  // $scope.currentDate = '';
-  // $scope.updateMyText = function(date) {
-  // $scope.myText = 'Selected';
-  // };
+    // $scope.myText = 'Not Selected';
+    // $scope.currentDate = '';
+    // $scope.updateMyText = function(date) {
+    // $scope.myText = 'Selected';
+    // };
 
-  var invoice = {
-    total: 0,
-    quantity: 0,
-    cost: 0,
-    calculateInvoiceTotal: function(quantity, cost, event){
-      this.total = InvoiceSvc.calculateInvoiceTotal(quantity, cost);
+    var invoice = {
+      total: 0,
+      quantity: 0,
+      cost: 0,
+      calculateInvoiceTotal: function (quantity, cost, event) {
+        this.total = InvoiceSvc.calculateInvoiceTotal(quantity, cost);
 
-      console.log(event);
-    }
-  };
-  $scope.invoice = invoice;
-
-  
-  $scope.appName = SharedSvc.appName;
-
-  $scope.person = {
-    name: "omar",
-    age: 30
-  };
-
-  // search result directive model
-  $scope.searchResult = {
-    heading: "Search Result Heading",
-    text: "Search Result Text"
-  };
-
-  $scope.formatSearchResult = function(searchResult){
-
-    return `${searchResult.heading} - ${searchResult.text}`;
-
-  };
+        console.log(event);
+      }
+    };
+    $scope.invoice = invoice;
 
 
-}]);
+    $scope.appName = SharedSvc.appName;
+
+    $scope.person = {
+      name: "omar",
+      age: 30
+    };
+
+    // search result directive model
+    $scope.searchResults = [
+      {
+        heading: "Search Result Heading 1",
+        text: "Search Result Text 1"
+      },
+      {
+        heading: "Search Result Heading 2",
+        text: "Search Result Text 2"
+      },
+      {
+        heading: "Search Result Heading 3",
+        text: "Search Result Text 3"
+      },
+    ];
+
+    $scope.formatSearchResult = function (searchResult) {
+
+      return `${searchResult.heading} - ${searchResult.text}`;
+
+    };
+
+
+  }]);
