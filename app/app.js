422f60faf96ec2eb62ae5e69a96531ffb7a21c27
Mahmoud Khattab <m_khatab_88@hotmail.com>
2017-10-08 13:05:37 +0400

    wrap app.js in IIFE

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+(angular.module('myApp', [
   'ngRoute',
   'myApp.services',
   'myApp.directives',
@@ -14,4 +14,4 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/home'});
-}]);
+}]))();
